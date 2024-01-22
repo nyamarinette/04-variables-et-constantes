63729b6dfb0d5f5a32f587807594ac1f913f34b5
nyamarinette <nyamarinette2020@gmail.com>
2024-01-22 19:16:24 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+/*
+        int ageUtilisateur  = 0;
+        System.out.println(ageUtilisateur);
+
+        ageUtilisateur = 30;
+        System.out.println(ageUtilisateur);
+        }
+    }/*
+
+ */
+        //Je vous présente la creation d'une constante
+        final int ONE_NUMBER = 35;
+        System.out.println(ONE_NUMBER);
+
+    }
+}
+
